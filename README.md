7b978de8cd813528ec77b0c2d3a7d5a777dcb73e
adic-9 <adic_9@outlook.com>
2023-06-19 11:51:52 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# hn-clusters
+
+This is a simple script that takes the hn content you follow and graphs it so you can see trends.
+
+#### features that would be fun but i'm not going to implement unless i have an explicit reason to
+- tagging data from different devices in different colors
+- tagging data from different people in different colors (potentially against an "average" if I ever host this somewhere
---
+++ clusters.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import mplcursors
+import spacy
+
+#can use en_core_web_sm instead for faster results
+
+nlp = spacy.load('en_core_web_lg')
+embs = []
+labels = []
+
+with open("hn_history.txt", "r") as f:
+        phrases = f.read().splitlines()
+
+        for phrase in phrases:
+            emb = [token.vector for token in nlp(phrase)]
+            emb = np.mean(emb, axis=0)
+            if emb.any():
+                embs.append(emb)
+                labels.append(phrase)
+
+embs = np.array(embs)
+tsne = TSNE(n_components=2)
+X_tsne = tsne.fit_transform(embs)
+
+scatter = plt.scatter(X_tsne[:, 0], X_tsne[:, 1])
+cursor = mplcursors.cursor(scatter, hover=True)
+
+def label_formatter(sel):
+    index = sel.target.index
+    x, y = sel.target
+    label = labels[index]
+    sel.annotation.set_text(label)
+    sel.annotation.xy = (x, y)
+
+cursor.connect("add", label_formatter)
+
+plt.show()
+
